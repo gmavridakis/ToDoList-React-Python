faf02937191cb34bc153b45e41e51f73d399ac6f
gmavridakis <greg.mavridakhs@gmail.com>
2020-02-02 18:41:26 +0200

    init frontend

---
+++ frontend/todo-list-front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TasksUI from './component/TasksUI'
+
+function App() {
+  return (
+    <div className="App">
+      <TasksUI></TasksUI>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/todo-list-front/src/component/TasksUI.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TasksUI = () => {
+    return <h1>This will be the main component</h1>
+}
+
+export default TasksUI
